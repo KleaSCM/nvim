3380f3a032c2c41daec1192ebb271ee4076a6f45
klea <klea@localhost>
2025-12-16 12:52:28 +0900

    refactor: enhance nvim-cmp configuration with error handling and improved mappings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,9 +7,60 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "saadparwaiz1/cmp_luasnip",
       "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
     },
     opts = function()
-      local cmp = require("cmp")
+      local ok_cmp, cmp = pcall(require, "cmp")
+      if not ok_cmp then
+        return {}
+      end
+      local ok_snip, luasnip = pcall(require, "luasnip")
+      if not ok_snip then
+        luasnip = nil
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            if luasnip then
+              require("luasnip").lsp_expand(args.body)
+            end
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip and luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip and luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "codeium" },
+        }, { { name = "buffer" }, { name = "path" } }),
+      })
+
       return {}
     end,
   },
